b4bebb706762eeb3d9bd3371a5f0d511996aa0c0
Evan <evanlin275282@gmail.com>
2026-01-16 21:16:31 -0800

    Basic web

---
+++ storyboard-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'; // optional separate CSS file
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1>Welcome to Storyboard</h1>
+      <h1>Create your alternate history!</h1>
+    </div>
+  );
+}
+
+export default App;
